f162c2f54c9120830a963c59d2e21f86babf5b3d
Big mike <mikendirangu26@gmail.com>
2025-09-20 11:54:17 +0300

    Completed Sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,3 +2,14 @@
 
 def print_fibonacci(length):
     pass
+    if length <= 0:
+        print([])          # empty list when length is 0 or negative
+        return
+
+    # start the sequence
+    fib = [0, 1]
+    while len(fib) < length:
+        fib.append(fib[-1] + fib[-2])
+
+    # slice to requested length and print
+    print(fib[:length])    
